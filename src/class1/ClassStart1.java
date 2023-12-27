eea98ea7a2f1eb4705eccfd27a8184e7acff3bd9
syk25 <syk2245@gmail.com>
2023-12-27 13:18:40 +0900

    study: 클래스

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,23 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 18;
+        int student2Grade = 60;
+
+        String student3Name = "학생3";
+        int student3Age = 13;
+        int student3Grade = 50;
+
+        System.out.println("이름: " + student1Name + "나이: " + student1Age + " 성적 " + student1Grade);
+        System.out.println("이름: " + student2Name + "나이: " + student2Age + " 성적 " + student2Grade);
+        System.out.println("이름: " + student3Name + "나이: " + student3Age + " 성적 " + student3Grade);
+
+
+    }
+}
---
+++ src/class1/ClassStart2.java
@@ -0,0 +1,12 @@
+package class1;
+
+public class ClassStart2 {
+    public static void main(String[] args) {
+        String[] studentNames = {"학생1", "학생2","학생3"};
+        int[] studentAges = {15, 18, 13};
+        int[] studentGrades = {90, 60, 50};
+        for (int i = 0; i < 3; i++) {
+            System.out.println("이름: " + studentNames[i] + " 나이: " + studentAges[i] + " 성적: " + studentGrades[i]);
+        }
+    }
+}
