0561d3fe7907f22ffb245fcdb6cef871ea9f05fa
syk25 <syk2245@gmail.com>
2023-12-29 02:14:35 +0900

    update: access modifier

--- src/access/b/PublicClassOuterMain.java
+++ src/access/b/PublicClassOuterMain.java
@@ -1,7 +1,5 @@
 package access.b;
 
-import access.a.DefaultClass1;
-import access.a.DefaultClass2;
 import access.a.PublicClass;
 
 public class PublicClassOuterMain {
@@ -9,7 +7,7 @@ public class PublicClassOuterMain {
         PublicClass publicClass = new PublicClass();
 
         // 다른 패키지에서는 default class 접근 불가
-        DefaultClass1 defaultClass1 = new DefaultClass1();
-        DefaultClass2 defaultClass1 = new DefaultClass2();
+//        DefaultClass1 defaultClass1 = new DefaultClass1();
+//        DefaultClass2 defaultClass1 = new DefaultClass2();
     }
 }
